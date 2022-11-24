f9ef47edf8f2339b874269745e231e93356018f3
Mohammad Barati <m.mohammadbarati.b@gmail.com>
2022-11-24 23:18:30 +0330

    Create footer and Update readme.md

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <NextNProgress height={5} color={"#3b82f6"} />
       <Component {...pageProps} />
+      <Footer />
     </SessionProvider>
   );
 }
