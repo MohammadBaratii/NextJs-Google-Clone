30912b26e411ddf5e29ba42cc3bad83c96b1fa32
Mohammad Barati <m.mohammadbarati.b@gmail.com>
2022-12-03 00:41:05 +0330

    Add sign in with google option

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,20 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  // For some reason, Google provider doesn't work. So I used Github provider instead.
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  secret: process.env.JWT_SECRET,
   pages: {
     signIn: "/auth/signin",
   },
+  secret: process.env.JWT_SECRET,
 });
